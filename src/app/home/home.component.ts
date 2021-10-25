741e19623ab84d5b8a89becdf2d81476e194249f
Paul Welby <pwelby@gmail.com>
2021-10-24 19:23:10 -0700

    Changed title to Follower Application and added subtitle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title = "Angular App";
-  
+  title = 'Followers Application';
+  subtitle = 'Followers of Mash Hamedani on Youtube';
+
   constructor() { }
 
   ngOnInit() {
